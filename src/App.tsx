32c946e64a54353175e4f7ebb54506a1bd0324e6
ZzzandyzzZ <andy_gean@hotmail.com>
2023-11-17 19:25:44 -0500

    Setup project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Grid } from './components/grid'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold">Shortest Path algorithms</h1>
+      <Grid />
+    </>
+  )
+}
+
+export default App
