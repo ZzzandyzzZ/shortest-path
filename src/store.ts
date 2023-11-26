dea572af374718ad934c3113a6873036b6adde3d
ZzzandyzzZ <andy_gean@hotmail.com>
2023-11-26 08:11:59 -0500

    Add start and end coord to store

--- src/store.ts
+++ src/store.ts
@@ -1,30 +1,37 @@
 import { create } from 'zustand'
 
-import { type Node } from './types'
+import { type Coord, type Node } from './types'
 
 interface Store {
   gridRows: number
   gridColumns: number
   grid: Node[][]
+  startCoord: Coord
+  endCoord: Coord
 }
-const initialGridRows = 20
-const initialGridColumns = 40
+
+const startCoord = { i: 0, j: 0 }
+const endCoord = { i: 0, j: 0 }
+const gridRows = 20
+const gridColumns = 40
 const initalNode = {
   visited: false,
   partOfSolution: false,
   blocked: false,
   distance: Infinity
 }
-const initialGrid = Array(initialGridRows).fill(null).map(
-  (_, i) => Array(initialGridColumns).fill(null).map(
+const initialGrid = Array(gridRows).fill(null).map(
+  (_, i) => Array(gridColumns).fill(null).map(
     (_, j) => ({ ...initalNode, coord: { i, j } })
   )
 )
 
 export const useStore = create<Store>((set) => ({
   grid: initialGrid,
-  gridRows: initialGridRows,
-  gridColumns: initialGridColumns,
+  gridRows,
+  gridColumns,
+  startCoord,
+  endCoord,
   resetGrid: () => {
     set({ grid: initialGrid })
   },
