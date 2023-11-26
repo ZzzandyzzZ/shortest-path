561a1d701f7532071959f739c55e384ef7a9fbdd
ZzzandyzzZ <andy_gean@hotmail.com>
2023-11-26 08:59:58 -0500

    add resetGrid method to store

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,9 @@
 import { create } from 'zustand'
 
+import { getRandomString } from './lib'
+
 import { type Coord, type Node } from './types'
+import { generateRandomGrid } from './utils/generate-random-grid'
 
 interface Store {
   gridRows: number
@@ -8,12 +11,14 @@ interface Store {
   grid: Node[][]
   startCoord: Coord
   endCoord: Coord
+  seed: string
   resetGrid: () => void
   setGridRows: (rows: number) => void
   setGridColumns: (columns: number) => void
   blockNode: ({ i, j }: Coord) => void
 }
 
+const seed = getRandomString()
 const startCoord = { i: 0, j: 0 }
 const endCoord = { i: 0, j: 0 }
 const gridRows = 6
@@ -36,8 +41,17 @@ export const useStore = create<Store>((set, get) => ({
   gridColumns,
   startCoord,
   endCoord,
+  seed,
   resetGrid: () => {
-    set({ grid: initialGrid })
+    const startCoord = get().startCoord
+    const endCoord = get().endCoord
+    const seed = get().seed
+    const tempGrid = generateRandomGrid({ initialGrid, seed })
+    tempGrid[startCoord.i][startCoord.j].distance = 0
+    tempGrid[startCoord.i][startCoord.j].visited = true
+    tempGrid[startCoord.i][startCoord.j].blocked = false
+    tempGrid[endCoord.i][endCoord.j].blocked = false
+    set({ grid: tempGrid })
   },
   setGridRows: (rows: number) => {
     set({ gridRows: rows })
