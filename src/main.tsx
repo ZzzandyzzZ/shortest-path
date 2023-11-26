09352f9081cdbebcfd9ddbfb8e6c06a468468450
ZzzandyzzZ <andy_gean@hotmail.com>
2023-11-26 09:01:12 -0500

    Fix eslint at app file

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import {
   createBrowserRouter,
   RouterProvider
 } from 'react-router-dom'
-import App from './App.tsx'
+import App from './app.tsx'
 import './index.css'
 
 const router = createBrowserRouter([
