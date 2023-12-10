aea030e4acc0f65b9116288bd951fa3690a31f88
ZzzandyzzZ <andy_gean@hotmail.com>
2023-12-10 17:48:37 -0500

    use React.memo in GridCell

--- src/components/grid-cell.tsx
+++ src/components/grid-cell.tsx
@@ -1,24 +1,27 @@
+import { memo } from 'react'
 import { useStore } from '../store'
 
-import { type Coord } from '../types'
+import type { Node } from '../types'
 
 interface Props {
-  coord: Coord
+  i: number
+  j: number
+  isCurrNode: boolean
+  isMousePressed: boolean
+  node: Node
 }
-export const GridCell = ({ coord: { i, j } }: Props) => {
-  const grid = useStore(state => state.grid)
+
+export const GridCell = memo(({ i, j, isCurrNode, isMousePressed, node: { partOfSolution, visited, blocked } }: Props) => {
   const startCoord = useStore(state => state.startCoord)
   const endCoord = useStore(state => state.endCoord)
-  const currNode = useStore(state => state.currNode)
-  const isMousePressed = useStore(state => state.isMousePressed)
   const blockNode = useStore(state => state.blockNode)
   const getBgColor = () => {
-    if (grid[i][j].blocked) return 'bg-black'
+    if (blocked) return 'bg-black'
     if (i === startCoord.i && j === startCoord.j) return 'bg-green-600'
     if (i === endCoord.i && j === endCoord.j) return 'bg-red-600'
-    if (currNode?.coord.i === i && currNode?.coord.j === j) return 'bg-orange-400'
-    if (grid[i][j].partOfSolution) return 'bg-blue-600'
-    if (grid[i][j].visited) return 'bg-green-300'
+    if (isCurrNode) return 'bg-orange-400'
+    if (partOfSolution) return 'bg-blue-600'
+    if (visited) return 'bg-green-300'
     return 'bg-blue-100'
   }
 
@@ -31,4 +34,4 @@ export const GridCell = ({ coord: { i, j } }: Props) => {
     {/* {gridItems[i][j].distance} */}
     {/* {i}-{j} */}
   </td>
-}
+})
