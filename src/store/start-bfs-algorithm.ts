877ed68372aaed5a0c8f06a3ea997f835730e7c0
ZzzandyzzZ <andy_gean@hotmail.com>
2023-12-17 13:05:52 -0500

    Fix startBfsAlgorithm

--- src/store/start-bfs-algorithm.ts
+++ src/store/start-bfs-algorithm.ts
@@ -1,8 +1,10 @@
 import { useStore } from '.'
 import { sleep } from '../lib'
+import { getGridSize } from '../utils'
 
 export const startBfsAlgorithm = async () => {
-  const { startCoord, gridColumns, gridRows, grid, visitNode, setCurrNode, setIsRunning } = useStore.getState()
+  const { startCoord, grid, visitNode, setCurrNode, setIsRunning } = useStore.getState()
+  const { gridCols, gridRows } = getGridSize(grid)
   setIsRunning(true)
   const queue = [grid[startCoord.i][startCoord.j]]
   while (queue.length >= 0) {
@@ -20,7 +22,7 @@ export const startBfsAlgorithm = async () => {
     ]
     directions.forEach(({ i: ci, j: cj }) => {
       const { grid } = useStore.getState()
-      if (ci >= gridRows || cj >= gridColumns || ci < 0 || cj < 0) return
+      if (ci >= gridRows || cj >= gridCols || ci < 0 || cj < 0) return
       const children = grid[ci][cj]
       if (children.visited || children.blocked) return
       visitNode({ i: ci, j: cj, currNode })
