efe1b6e9cef73aa36ae9e2ee54cd3fe4fa2854ca
ZzzandyzzZ <andy_gean@hotmail.com>
2023-12-17 15:12:03 -0500

    update global types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,10 +1,11 @@
+export type Grid = Node[][]
 export interface Node {
   visited: boolean
   blocked: boolean
   partOfSolution: boolean
   distance: number
   coord: Coord
-  prevCoord?: Coord
+  prevCoord?: Coord | null
 }
 
 export interface Coord {
