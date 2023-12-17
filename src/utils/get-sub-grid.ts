5b5f1f558dc1fd70b418efea1cedadda261b47cd
ZzzandyzzZ <andy_gean@hotmail.com>
2023-12-17 12:22:07 -0500

    Update start and end point in getSubGrid

--- src/utils/get-sub-grid.ts
+++ src/utils/get-sub-grid.ts
@@ -14,5 +14,15 @@ export const getSubGrid = ({ baseGrid, gridCols, gridRows }: Props) => {
   const maxCols = baseGrid[0].length
   const rowStart = maxRows / 2 - gridRows / 2
   const colStart = maxCols / 2 - gridCols / 2
-  return baseGrid.slice(rowStart, rowStart + gridRows).map(row => row.slice(colStart, colStart + gridCols))
+  const subgrid = baseGrid
+    .slice(rowStart, rowStart + gridRows)
+    .map(row => row.slice(colStart, colStart + gridCols))
+    .map(row => row.map(cell => ({ ...cell }))) // Deep copy
+  const startCoord = { i: 0, j: 0 }
+  const endCord = { i: gridRows - 1, j: gridCols - 1 }
+  subgrid[startCoord.i][startCoord.j].distance = 0
+  subgrid[startCoord.i][startCoord.j].visited = true
+  subgrid[startCoord.i][startCoord.j].blocked = false
+  subgrid[endCord.i][endCord.j].blocked = false
+  return subgrid
 }
