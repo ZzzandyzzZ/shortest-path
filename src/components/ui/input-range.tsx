580c0f0b3dd59dd3c4858c8021740126a27cee35
ZzzandyzzZ <andy_gean@hotmail.com>
2023-12-17 09:08:09 -0500

    add max value prop to InputRange

--- src/components/ui/input-range.tsx
+++ src/components/ui/input-range.tsx
@@ -1,12 +1,11 @@
-import { MAX_NUMBER_ROW } from '../../constants'
-
 interface Props {
   textLabel: string
   initalValue: number
+  maxValue: number
   handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-export const InputRange = ({ textLabel, initalValue, handleChange }: Props) => {
+export const InputRange = ({ textLabel, initalValue, handleChange, maxValue }: Props) => {
   return (
     <div>
       <label htmlFor="steps-range" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{textLabel}</label>
@@ -14,7 +13,7 @@ export const InputRange = ({ textLabel, initalValue, handleChange }: Props) => {
         id="steps-range"
         type="range"
         min="4"
-        max={MAX_NUMBER_ROW}
+        max={maxValue}
         value={initalValue}
         step="2"
         onChange={handleChange}
