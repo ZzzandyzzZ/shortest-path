26fd39c6c434de9c503159c275cf30b369ecea6d
ZzzandyzzZ <andy_gean@hotmail.com>
2024-01-01 10:14:33 -0500

    add new pallete color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        base: '#0B2447',
+        subbase: '#19376D',
+        contrast: '#52649A',
+        bright: '#A5D7E8'
+      }
+    },
   },
   plugins: [],
 };
