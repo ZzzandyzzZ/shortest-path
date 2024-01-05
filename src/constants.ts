a965420246241bcc662b85b84eaae238c13b4881
ZzzandyzzZ <andy_gean@hotmail.com>
2024-01-04 19:40:15 -0500

    update MAX_NUMBER_ROW and MAX_NUMBER_COL constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const MAX_NUMBER_ROW = 42
-export const MAX_NUMBER_COL = 66
+export const MAX_NUMBER_ROW = 30
+export const MAX_NUMBER_COL = 55
 export const DEFAULT_GRID_ROWS = 20
 export const DEFAULT_GRID_COLS = 15
