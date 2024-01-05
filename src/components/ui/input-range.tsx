eb65a1b839ac38decc741b846c1679573624a922
ZzzandyzzZ <andy_gean@hotmail.com>
2024-01-05 18:51:15 -0500

    add shdow to input range

--- src/components/ui/input-range.tsx
+++ src/components/ui/input-range.tsx
@@ -9,7 +9,7 @@ const minValue = 4 // We want a 4x4 min grid size
 
 export const InputRange = ({ textLabel, initalValue, handleChange, maxValue }: Props) => {
   return (
-    <label className="text-sm text-center">
+    <label className="text-center bg-lazuli-500 rounded-full px-5 py-2.5">
       {textLabel}: {initalValue}
       <div className='flex items-center gap-2'>
         {minValue}
@@ -21,7 +21,7 @@ export const InputRange = ({ textLabel, initalValue, handleChange, maxValue }: P
           value={initalValue}
           step="2"
           onChange={handleChange}
-          className="w-full h-2 rounded-lg appearance-none cursor-pointer accent-lazuli-400"
+          className="w-full h-2 rounded-lg appearance-none cursor-pointer accent-lazuli-900"
         />
         {maxValue}
       </div>
