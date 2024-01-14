d706680001273dbaf5ff8d3e6f6a930426f9cbb1
ZzzandyzzZ <andy_gean@hotmail.com>
2024-01-14 13:54:25 -0500

    add cursor pointer to buttons

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -8,7 +8,7 @@ interface Props {
 export const Button = ({ onClick, children }: Props) => {
   return (
     <button
-      className='bg-lazuli-500 rounded-full font-semibold px-5 py-2.5 text-center shadow-lazuli500 active:shadow-none active:translate-y-1 w-full peer'
+      className='cursor-pointer bg-lazuli-500 rounded-full font-semibold px-5 py-2.5 text-center shadow-lazuli500 active:shadow-none active:translate-y-1 w-full peer'
       onClick={onClick}
     >
       {children}
