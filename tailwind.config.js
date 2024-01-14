b1bd3ea0d20e3efce02e471d189c1931b7f3b76a
ZzzandyzzZ <andy_gean@hotmail.com>
2024-01-13 20:45:21 -0500

    extender tailwind config to add new cursor

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      cursor: {
+        default: "url(assets/cursors/default.svg), default",
+        pointer: "url(assets/cursors/pointer.svg), pointer",
+        remove: "url(assets/cursors/remove.svg), default",
+        add: "url(assets/cursors/add.svg), default",
+      },
       colors: {
         /** https://coolors.co/002147-15325c-294371-3e5486-52649a-6781ae-7c9ec1-91bbd5-a5d7e8-dff1f7 **/
         lazuli: {
